0f8f999c8b940230000b53c426c5627c45af774a
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-13 01:49:10 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,14 @@
 class Person
   def name
-    @name
+    @name 
   end 
   def name= (name)
-    @name
+    @name = name
   end 
   def job
     @job
   end  
   def job= (job)
-    @job
+    @job = job
   end  
 end   
